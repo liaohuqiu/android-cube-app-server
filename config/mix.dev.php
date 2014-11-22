bc2fca06d21efdc4bca78aae4c50cf88292281ec
liaohuqiu <liaohuqiu@gmail.com>
2014-11-22 12:31:31 +0800

    disable admin for prod

--- config/mix.dev.php
+++ config/mix.dev.php
@@ -1,6 +1,5 @@
 <?php
 $data = array();
-$data['disable_admin'] = true;
 $data['s_host'] = 's.android-cube-app-server.test.srain.in';      // change to your resource host
 $data['mcache-servers'] = array(
     array('127.0.0.1', 11211),
--- config/mix.prod.php
+++ config/mix.prod.php
@@ -1,5 +1,6 @@
 <?php
 $data = array();
+$data['disable_admin'] = true;
 $data['s_host'] = 's.android-cube-app-server.liaohuqiu.net';      // change to your resource host
 $data['mcache-servers'] = array(
     array('127.0.0.1', 11211),
